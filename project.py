db8ed2c0d5219994fa38cad2b801fe424d702b1b
AarushSingh2706 <nisha2008mb0035@gmail.com>
2025-06-08 17:07:45 +0530

    project

---
+++ project.py
@@ -0,0 +1,7 @@
+a = str(input("Raining or Summer")) 
+if a == "Raining" : 
+    print("Dont go out to play")
+elif a == "Summer" : 
+    print("You can go out to play")
+else : 
+    print("Please enter the given two options")
