bc4b5466b171fc9a0039b2271a60fc3dd017b2ff
AarushSingh2706 <nisha2008mb0035@gmail.com>
2025-06-08 16:57:44 +0530

    greater than

---
+++ greater than.py
@@ -0,0 +1,8 @@
+a = float(input("Give any number"))
+if a > 15 : 
+    print("The number is greater than 15")
+    print("I am in if block")
+else : 
+    print("The number is smaller than 15")
+    print("I am in else block")
+print("I am neither in else block nor in if block")
